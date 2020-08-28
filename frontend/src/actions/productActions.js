2ad7bea3dd8dd9763c02690ad76a3b4cecdabb2f
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2020-08-27 20:16:20 -0400

    Connect Product screen with Redux

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,6 +3,11 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
 } from "../constants/productConstants";
+import {
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+} from "../constants/productConstants";
 import axios from "axios";
 
 const listProducts = () => async (dispatch) => {
@@ -15,4 +20,13 @@ const listProducts = () => async (dispatch) => {
   }
 };
 
-export { listProducts };
+const detailsProduct = (productId) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
+    const { data } = await axios.get("/api/products/" + productId);
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: PRODUCT_DETAILS_FAIL, payload: error.message });
+  }
+};
+export { listProducts, detailsProduct };
