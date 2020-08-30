d3820eab0b343cd3be975929c2363dccbff505eb
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2020-08-30 19:53:54 -0400

    cart Screen

---
+++ frontend/src/Screens/CartScreen.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import { addToCart } from "../actions/cartActions";
+import { useDispatch, useSelector } from "react-redux";
+
+function CartScreen(props) {
+  const productId = props.match.params.id;
+  const qty = props.location.search
+    ? Number(props.location.search).split("=")[1]
+    : 1;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, []);
+  return <div>Cart Screen</div>;
+}
+export default CartScreen;
