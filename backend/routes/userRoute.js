6ee4688a6395de8259cf7c40e166a9c2a5004090
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2020-09-15 00:41:11 -0400

    User Profile Screen

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,26 +1,26 @@
 import express from "express";
 import User from "../models/userModel";
-import { getToken } from "../util";
+import { getToken, isAuth } from "../util";
 
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
-  const newUser = await user.save();
-  if (newUser) {
+router.put("/:id", isAuth, async (req, res) => {
+  const userId = req.params.id;
+  const user = await User.findById(userId);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.password = req.body.password || user.password;
+    const updatedUser = await user.save();
     res.send({
-      _id: newUser.id,
-      name: newUser.name,
-      email: newUser.email,
-      isAdmin: newUser.isAdmin,
-      token: getToken(newUser),
+      _id: updatedUser.id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: getToken(updatedUser),
     });
   } else {
-    res.status(401).send({ msg: "Invalid User Data" });
+    res.status(404).send({ msg: "User Not Found" });
   }
 });
 
@@ -39,6 +39,26 @@ router.post("/signin", async (req, res) => {
     });
   } else {
     res.status(401).send({ msg: "Invalid Email or Password" });
+  }
+});
+
+router.post("/register", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  const newUser = await user.save();
+  if (newUser) {
+    res.send({
+      _id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+      token: getToken(newUser),
+    });
+  } else {
+    res.status(401).send({ msg: "Invalid User Data" });
   }
 });
 
