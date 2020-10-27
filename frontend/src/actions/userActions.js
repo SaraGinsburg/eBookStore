3872601af869b291dbf8d98216d4dc50691986cc
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2020-10-27 01:18:28 -0400

    implement-order- payment

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -26,7 +26,7 @@ export const signin = (email, password) => async (dispatch) => {
   }
 };
 export const register = (name, email, password) => async (dispatch) => {
-  dispatch({ type: USER_REGISTER_REQUEST, payload: { name, email, password } });
+  dispatch({ type: USER_REGISTER_REQUEST, payload: {  email, password } });
   try {
     const { data } = await Axios.post("/api/users/register", {
       name,
