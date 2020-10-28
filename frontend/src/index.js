0c5cace97e236c03423ca8c7aeb017188cd752a0
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2020-10-28 16:42:41 -0400

    Create admin view

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,12 +8,19 @@ import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </Provider>,
   document.getElementById("root")
 );
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   </Provider>,
+//   document.getElementById("root")
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
