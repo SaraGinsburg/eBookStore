99899f08775eacd26906b3ec263acf2a63358a38
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2020-11-09 19:50:19 -0500

    allow signin after user logsout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -33,7 +33,7 @@ export const signin = (email, password) => async (dispatch) => {
 };
 
 export const register = (name, email, password) => async (dispatch) => {
-  dispatch({ type: USER_REGISTER_REQUEST, payload: {  email, password } });
+  dispatch({ type: USER_REGISTER_REQUEST, payload: { email, password } });
   try {
     const { data } = await Axios.post("/api/users/register", {
       name,
@@ -56,43 +56,47 @@ export const register = (name, email, password) => async (dispatch) => {
 
 export const signout = () => (dispatch) => {
   localStorage.removeItem("userInfo");
-  localStorage.removeItem("ucartItems");
+  localStorage.removeItem("cartItems");
   localStorage.removeItem("shippingAddress");
   dispatch({ type: USER_SIGNOUT });
+  document.location.location.href = "/signin";
 };
 
 export const detailsUser = (userId) => async (dispatch, getState) => {
-  dispatch({ type: USER_DETAILS_REQUEST, payload: userId});
-  const { userSignin:{userInfo}} = getState();   //get token from getState()
+  dispatch({ type: USER_DETAILS_REQUEST, payload: userId });
+  const {
+    userSignin: { userInfo },
+  } = getState(); //get token from getState()
   try {
     const { data } = await Axios.get(`/api/users/${userId}`, {
-      headers: {Authorization: `Bearer ${userInfo.token}`}
-    })
-    dispatch({type: USER_DETAILS_SUCCESS, payload: data});
+      headers: { Authorization: `Bearer ${userInfo.token}` },
+    });
+    dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
   } catch (error) {
-    const message = 
-    error.response && error.response.data.message
-    ? error.response.data.message
-    : error.message;
-    dispatch({type: USER_DETAILS_FAIL, payload: message });
-    
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    dispatch({ type: USER_DETAILS_FAIL, payload: message });
   }
-}
+};
 export const updateUserProfile = (user) => async (dispatch, getState) => {
-  dispatch({type: USER_UPDATE_PROFILE_REQUEST, payload: user});
-  const { userSignin: {userInfo},} = getState();
+  dispatch({ type: USER_UPDATE_PROFILE_REQUEST, payload: user });
+  const {
+    userSignin: { userInfo },
+  } = getState();
   try {
     const { data } = await Axios.put(`/api/users/profile`, user, {
-      headers: { Authorization: `Bearer ${userInfo.token}`}
-    })
-    dispatch({type: USER_UPDATE_PROFILE_SUCCESS, payload: data});
-    dispatch({ type: USER_SIGNIN_SUCCESS, payload: data })
-    localStorage.setItem('userInfo', JSON.stringify(data))
+      headers: { Authorization: `Bearer ${userInfo.token}` },
+    });
+    dispatch({ type: USER_UPDATE_PROFILE_SUCCESS, payload: data });
+    dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
+    localStorage.setItem("userInfo", JSON.stringify(data));
   } catch (error) {
-    const message = 
+    const message =
       error.response && error.response.data.message
-      ? error.response.data.message
-      : error.message;
-    dispatch({type: USER_UPDATE_PROFILE_FAIL, payload: message})
+        ? error.response.data.message
+        : error.message;
+    dispatch({ type: USER_UPDATE_PROFILE_FAIL, payload: message });
   }
-}
+};
