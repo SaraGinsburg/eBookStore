39a575dd9a76674e3ba55df2efbe87a7cb7800f0
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2020-11-11 21:45:12 -0500

    modify orderRouter

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -1,17 +1,64 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import Order from "../models/orderModel.js";
-import { isAdmin, isAuth } from "../utils.js";
+import User from "../models/userModel.js";
+import { isAuth, isSeller, isAdmin } from "../utils.js";
+import Product from "../models/productModel.js";
 
 const orderRouter = express.Router();
 
 orderRouter.get(
   "/",
   isAuth,
+  isSeller,
+  expressAsyncHandler(async (req, res) => {
+    const seller = req.query.seller ? { seller: req.query.seller } : {};
+    const orders = await Order.find({ ...seller })
+      .populate("user", "name")
+      .populate("seller", "name");
+    res.send(orders);
+  })
+);
+orderRouter.get(
+  "/summary",
+  isAuth,
   isAdmin,
   expressAsyncHandler(async (req, res) => {
-    const orders = await Order.find({}).populate("user", "name");
-    res.send(orders);
+    const orders = await Order.aggregate([
+      {
+        $group: {
+          _id: null,
+          numOrders: { $sum: 1 },
+          totalSales: { $sum: "$totalPrice" },
+        },
+      },
+    ]);
+    const users = await User.aggregate([
+      {
+        $group: {
+          _id: null,
+          numUsers: { $sum: 1 },
+        },
+      },
+    ]);
+    const dailyOrders = await Order.aggregate([
+      {
+        $group: {
+          _id: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
+          orders: { $sum: 1 },
+          sales: { $sum: "$totalPrice" },
+        },
+      },
+    ]);
+    const productCategories = await Product.aggregate([
+      {
+        $group: {
+          _id: "$category",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+    res.send({ users, orders, dailyOrders, productCategories });
   })
 );
 
@@ -24,41 +71,68 @@ orderRouter.get(
   })
 );
 
-orderRouter.post(
-  "/",
-  isAuth,
-  expressAsyncHandler(async (req, res) => {
-    if (req.body.orderItems.length === 0) {
-      res.status(400).send({ message: "Cart is empty" });
-    } else {
-      const order = new Order({
-        orderItems: req.body.orderItems,
-        shippingAddress: req.body.shippingAddress,
-        paymentMethod: req.body.paymentMethod,
-        itemsPrice: req.body.itemsPrice,
-        shippingPrice: req.body.shippingPrice,
-        taxPrice: req.body.taxPrice,
-        totalPrice: req.body.totalPrice,
-        user: req.user._id,
-      });
-      const createdOrder = await order.save();
-      res
-        .status(201)
-        .send({ message: "New Order Created", order: createdOrder });
-    }
-  })
-);
-
 orderRouter.get(
   "/:id",
   isAuth,
   expressAsyncHandler(async (req, res) => {
-    const order = await Order.findById(req.params.id);
+    const order = await Order.findOne({ _id: req.params.id }).populate(
+      "user",
+      "name email"
+    );
     if (order) {
       res.send(order);
     } else {
+      res.status(404).send("Order Not Found.");
+    }
+  })
+);
+
+orderRouter.delete(
+  "/:id",
+  isAuth,
+  isSeller,
+  expressAsyncHandler(async (req, res) => {
+    const order = Order.findOne({ _id: req.params.id });
+    if (order) {
+      const deleteOrder = await order.remove();
+      res.send(deleteOrder);
+    } else {
       res.status(404).send({ message: "Order Not Found" });
     }
+  })
+);
+
+orderRouter.post(
+  "/",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    if (!req.body.orderItems.length) {
+      res.status(400).send({ message: "Cart is empty" });
+    }
+    if (
+      !req.body.orderItems
+        .map((item) => item.seller)
+        .every((val) => val === req.body.orderItems[0].seller)
+    ) {
+      res.status(400).send({
+        message: "Multi Seller Error. Buy from one seller at a time.",
+      });
+    }
+    const newOrder = new Order({
+      orderItems: req.body.orderItems,
+      seller: req.body.orderItems[0].seller,
+      user: req.user._id,
+      shippingAddress: req.body.shippingAddress,
+      paymentMethod: req.body.paymentMethod,
+      itemsPrice: req.body.itemsPrice,
+      taxPrice: req.body.taxPrice,
+      shippingPrice: req.body.shippingPrice,
+      totalPrice: req.body.totalPrice,
+    });
+    const newOrderCreated = await newOrder.save();
+    res
+      .status(201)
+      .send({ message: "New Order Created", data: newOrderCreated });
   })
 );
 
@@ -84,25 +158,9 @@ orderRouter.put(
   })
 );
 
-orderRouter.delete(
-  "/:id",
-  isAuth,
-  isAdmin,
-  expressAsyncHandler(async (req, res) => {
-    const order = Order.findById(req.params.id);
-    if (order) {
-      const deleteOrder = await order.remove();
-      res.send({ message: "Order Deleted", order: deleteOrder });
-    } else {
-      res.status(404).send({ message: "Order Not Found" });
-    }
-  })
-);
-
 orderRouter.put(
   "/:id/deliver",
   isAuth,
-  isAdmin,
   expressAsyncHandler(async (req, res) => {
     const order = await Order.findById(req.params.id);
     if (order) {
